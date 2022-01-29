cbc4b5b0933b86351e6dcaa146bce094932075bd
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2022-01-29 13:04:25 -0500

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3">
+        <div class="card">
+            <img :src="producto.thumbnailUrl" alt="" class="card-img-top">
+            <div class="card-body">                
+                <h5 class="card-title">{{producto.title}}</h5>
+                <p class="card-text">$ <span>{{producto.precio}}</span></p>
+                <button class="btn btn-dark">Comprar</button>
+            </div>
+        </div>
+      </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Card',
+    props: {
+        producto: Object,
+    },
+   }
+
+</script>
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    productos: [],
   },
   mutations: {
+    setProducto(state, productos) {
+      state.productos = productos
+      console.log(state.productos)
+    },
+    getProductos(state) { 
+      return state.productos
+    }
   },
   actions: {
+    async fetchProducts({ commit }) {
+      try {
+        const response = await fetch('api.json')
+        const data = await response.json()
+        commit('setProducto', data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
   },
   modules: {
   }
